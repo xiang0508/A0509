47e024e59dda2cb240be0c4c79141f8c939b2cf6
xiang0508 <h2463088@mailst.cjcu.edu.tw>
2019-05-16 13:47:42 +0800

    add spinner(list)(fruit_name)

--- app/src/main/java/com/example/cjcu/a123/MainActivity.java
+++ app/src/main/java/com/example/cjcu/a123/MainActivity.java
@@ -2,17 +2,25 @@ package com.example.cjcu.a123;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner sp_fruit;
+    private Button button;
+
+
+    String[] fruit ={"papaya","apple","orange"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        Spinner sp =findViewById(R.id.sp);
+        button = findViewById(R.id.button);
+        sp_fruit = findViewById(R.id.sp_fruit);
 
+        ArrayAdapter adapter =new ArrayAdapter(this,android.R.layout.simple_list_item_1,fruit);
+        sp_fruit.setAdapter(adapter);
     }
 }
